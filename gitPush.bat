37b1da5f103068dbb48aa7e6c002e427aa46b635
mort1skoda <mort1skoda@gmail.com>
2023-01-18 12:39:30 +0100

    2023_01_18__12_39_30

--- gitPush.bat
+++ gitPush.bat
@@ -1,11 +1,10 @@
 git add --all
-git status
+::git status
 
 set datetimef=%date:~-4%_%date:~3,2%_%date:~0,2%__%time:~0,2%_%time:~3,2%_%time:~6,2%
-echo %datetimef%
 
 git commit -m "%datetimef%"
-git status
+::git status
 git push
 
 
